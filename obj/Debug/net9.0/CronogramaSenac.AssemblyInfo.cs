6e8fbbd4b3d12afb4624643e5dc01b13d2afe65d
bya123bya <byabyachile@gmail.com>
2025-12-03 22:00:32 -0300

    Adicionar Modal e cadastro UC

--- obj/Debug/net9.0/CronogramaSenac.AssemblyInfo.cs
+++ obj/Debug/net9.0/CronogramaSenac.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CronogramaSenac")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+841a132c9073638e4e131d56144921614702af06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ac1265bb819e0d77d5b742acd770e42af619c5b")]
 [assembly: System.Reflection.AssemblyProductAttribute("CronogramaSenac")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CronogramaSenac")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
